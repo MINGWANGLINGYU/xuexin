eff37ef5c9096fdb4710b1fd21aae5b096f5a274
MINGWANGLINGYU <3405873635@qq.com>
2026-04-20 18:43:55 +0800

    4.20

---
+++ src/database/seed/index.ts
@@ -0,0 +1,17 @@
+// src/database/seed/index.ts
+import { prisma } from "../client";
+
+import { createPostData } from "./post";
+
+async function seed() {
+  try {
+    await createPostData();
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+  await prisma.$disconnect();
+  process.exit();
+}
+
+seed();
